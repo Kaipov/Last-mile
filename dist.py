ea9cc84971b7d0443f844abe4723982be750b586
Ivan <ivan.kaipov@lacit.net>
2018-11-05 20:16:56 +0100

    Compute matrix distance and time. There is have errors

---
+++ dist.py
@@ -0,0 +1,48 @@
+from __future__ import print_function
+
+import time
+import swagger_client
+from swagger_client.rest import ApiException
+from pprint import pprint
+
+api_instance = swagger_client.MatrixApi()
+KEY = 'd297f285-9782-4f17-990a-fe9f0e71757c'  # str | Get your key at graphhopper.com
+
+#POINT = ['34.225239,-118.441814',
+#         '34.149773,-118.595022',
+#         '34.251001,-118.586668',
+#         '34.230391,-118.582597']
+
+OUT_ARRAY = ['distances', 'times']
+VEHICLE = 'car'
+
+
+def dist_matr():
+    pts_f = open("coords", "r")
+    l = pts_f.readline()[:-1]
+
+    point = []
+
+    while l:
+        point.append(str(l))
+        l = pts_f.readline()[:-1]
+
+    pts_f.close()
+
+    try:
+        # Matrix API
+        api_response = api_instance.matrix_get(KEY, point=point, out_array=OUT_ARRAY, vehicle=VEHICLE)
+        if api_response.distances is not None:
+            #f = open("matr.out", "w")
+            f = open("times", "w")
+            for t in api_response.times:
+                f.write(t + '/n')
+            f.close()
+        pprint(api_response)
+
+    except ApiException as e:
+        print("Exception when calling MatrixApi->matrix_get: %s\n" % e)
+
+
+dist_matr()
+
